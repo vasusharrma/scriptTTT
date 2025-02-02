a427f4a30df4a51703b7784ab39a5500f086f5e8
vasusharrma <sharmavashu099@gmail.com>
2025-02-02 14:16:42 +0530

    functions

---
+++ second/function.ts
@@ -0,0 +1,30 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUp(name: string, email: string, password: boolean) {
+
+  return name;
+}
+
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+
+
+addTwo(5);
+
+
+getUpper("vaSu Sharma");
+
+signUp("Vasu Sharma", "sharmavashu099@gmail.com", true);
+
+loginUser("V", "v@V.sh")
+
+export { }
