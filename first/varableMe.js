dda3c3f37b032009083af9faf965b59b102a0720
vasusharrma <sharmavashu099@gmail.com>
2025-02-02 11:23:20 +0530

    any

---
+++ first/varableMe.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Vasu";
+greetings = "aja";
+greetings.toLowerCase();
+console.log(greetings);
+/* greetings = 345; */
+var userId = 398494.93;
+// userId.numberMethods
+var isLoggedIn = false;
+// boolean.methods
+// good practice
+var roll = 3903;
+// roll = "HEllo "  error we can't assign string to a number
+// ANY type
+//
+var name;
+var isTrue;
+function getName() {
+    return "Vasu";
+}
+function getTrue() {
+    return true;
+}
+name = getName();
+isTrue = getTrue();
---
+++ first/varableMe.ts
@@ -0,0 +1,44 @@
+let greetings: string = "Hello Vasu";
+greetings = "aja";
+greetings.toLowerCase();
+console.log(greetings);
+/* greetings = 345; */
+
+let userId: number = 398494.93;
+// userId.numberMethods
+
+let isLoggedIn: boolean = false;
+
+// boolean.methods
+
+// good practice
+
+let roll = 3903;
+
+// roll = "HEllo "  error we can't assign string to a number
+
+
+// ANY type
+//
+
+let name;
+let isTrue: boolean;
+function getName() {
+  return "Vasu";
+}
+
+function getTrue() {
+  return true;
+}
+
+name = getName();
+isTrue = getTrue();
+
+
+
+
+export { }
+
+
+
+
