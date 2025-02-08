dccd83bfa9e584373973c6de1f8ef3a8dae0a73b
vasusharrma <sharmavashu099@gmail.com>
2025-02-08 13:01:18 +0530

    union arrays

--- third/union.ts
+++ third/union.ts
@@ -23,5 +23,19 @@ vasu = {
   id: 380
 }
 
+function getDb(id: number | string) {
+  //making api calls
+  if (typeof (id) === "string") {
+    id.toLowerCase();
+  }
+
+  if (typeof id === "number") {
+    id.toFixed(3);
+  }
 
 
+  console.log(`Db id is : ${id}`);
+}
+
+getDb(3);
+getDb("3");
